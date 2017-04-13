fc5d9f00d419563311d4bad594f40c705c9b5720
Hayden Luckenbach <hluckenb@gmail.com>
2017-04-13 18:03:43 -0500

    add indexes to columns

--- tweets/models.py
+++ tweets/models.py
@@ -5,8 +5,8 @@ class Tweet(models.Model):
     tweet_id = models.BigIntegerField()
     created_at = models.DateTimeField()
     text = models.TextField()
-    retweet_count = models.IntegerField()
+    retweet_count = models.IntegerField(db_index=True)
     handle = models.CharField(max_length=255)
-    media_type = models.CharField(null=True, max_length=255)
+    media_type = models.CharField(null=True, max_length=255, db_index=True)
 
     collected_at = models.DateTimeField(auto_now_add=True)
